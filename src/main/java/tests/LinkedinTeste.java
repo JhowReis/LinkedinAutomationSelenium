8cd27859ec61e5bac7edef6a94a17bd79313f7f7
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-03-01 23:56:38 -0300

    starting project

---
+++ src/main/java/tests/LinkedinTeste.java
@@ -0,0 +1,44 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import core.DSL;
+
+public class LinkedinTeste {
+	
+	private WebDriver driver;
+	private DSL dsl;
+	
+	@Before
+	public void start() {
+		System.setProperty("webdriver.chrome.driver", "/Users/jonat/Documents/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.linkedin.com/home");
+		dsl = new DSL(driver);
+		
+	}
+	
+	
+	@After
+	public void finish() {
+//		driver.quit();
+		
+	}
+	
+	
+	
+	@Test
+	public void login(){
+		dsl.clickButton("//a[contains(text(),'Sign in')]");
+		
+		
+		
+	}
+	
+
+}
